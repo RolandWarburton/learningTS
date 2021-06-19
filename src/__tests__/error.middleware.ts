e96e95b88a7cf9507f4eceadd07bad0f68252756
RolandWarburton <warburtonroland@gmail.com>
2021-06-19 14:44:43 +1000

    Update tests to be much better

--- src/__tests__/error.middleware.ts
+++ src/__tests__/error.middleware.ts
@@ -1,83 +1,83 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-// import request from "supertest";
-// import App from "../app";
-// import IndexRoute from "../routes/index.route";
-// import PageNotFound from "../routes/pageNotFound.route";
 import IndexController from "../controllers/index.controller";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 describe("test", () => {
+	// declare these partials outside of the test scope so they are available within the closure of the test at all times.
+	// A partial is a typescript feature that means the mocked object will be a subset of the object it is pretending to be
+	// 		Later on when we pass the mockRequest and mockResponse to the controller we coerce them into their
+	// 		type using the (mockRequest as Request), "as" keyword so the controller is happy with the types
 	let mockRequest: Partial<Request>;
 	let mockResponse: Partial<Response>;
-	// const nextFunction: NextFunction = jest.fn();
+	let nextFunction: Partial<NextFunction>;
+
+	// we also create these objects to store the result of the .status and .json that express gives to us when we mock
+	// 		the implementation of the .status and .json functions that exist inside the controller
+	let resultJson = {};
+	let resultStatus = {};
 
 	beforeEach(() => {
-		let responseObject = {};
+		// reset the request and response objects before each test
 		mockRequest = {};
-		mockResponse = {
-			json: jest.fn().mockImplementation((result) => {
-				responseObject = result;
-			}),
-		};
+		mockResponse = {};
+
+		// reset the objects that store results in them
+		resultJson = {};
+		resultStatus = {};
+
+		// The mockImplementation catches the .status function when it is called in express,
+		// 	then runs the below test code to "implement" what the function will do.
+		//
+		// To test express there is a special requirement, that is to return the "mockResponse" object every time,
+		// 		this mimics the "function chaining" behavior of express where we call .status().json({}) in the controller
+		//
+		// the .mockImplementation((result) => {} "result" variable is given to us by express and represents
+		// 		what the controller and express passed to the .status and .json parts of the chain,
+		// 		essentially we just steal that and keep it for our assertions later on in the test
+		mockResponse.status = jest.fn().mockImplementation((result) => {
+			resultStatus = result;
+			return mockResponse;
+		});
+
+		mockResponse.json = jest.fn().mockImplementation((result) => {
+			resultJson = result;
+			return mockResponse;
+		});
+
+		// Create a jest function that we can pass as the nextFunction to the controller
+		// 		If the controller reaches an error state, then the next() function will be called
+		// 		and we will be able to assert that by calling the .toHaveBeenCalledTimes() method
+		nextFunction = jest.fn();
 	});
 
-	test("test", () => {
-		let responseObject = {};
-		mockRequest = {};
-		mockResponse = {
-			json: jest.fn().mockImplementation((result) => {
-				responseObject = result;
-			}),
-		};
-
+	it("returns 200 and the expected json body", () => {
 		const controller = new IndexController().index;
-		controller(mockRequest as Request, mockResponse as Response);
-		expect(responseObject).toEqual({ hello: "world" });
+		// run the index controller with the req of {} and wait for the response
+		controller(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+		// when the res.status is called we expect it to be passed 200
+		expect(resultStatus).toBe(200);
+
+		// when the res.json is called we expect it to have the body json from the controller
+		expect(resultJson).toEqual({ hello: "world" });
+	});
+
+	it("calls the next function when the json error field is passed to it", () => {
+		const controller = new IndexController().index;
+
+		mockRequest.body = { error: "please" };
+		controller(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+		// check that the next function has been called
+		expect(nextFunction).toHaveBeenCalledTimes(1);
+	});
+
+	it("does not throw an error when json is passed to it", () => {
+		const controller = new IndexController().index;
+
+		mockRequest.body = { noError: "please" };
+		controller(mockRequest as Request, mockResponse as Response, nextFunction as NextFunction);
+
+		// check that the next function has been called
+		expect(nextFunction).toHaveBeenCalledTimes(0);
 	});
 });
-
-// // const mockRequest = {
-// // 	body: {
-// // 		firstName: "J",
-// // 		lastName: "Doe",
-// // 		email: "jdoe@abc123.com",
-// // 		password: "Abcd1234",
-// // 		passwordConfirm: "Abcd1234",
-// // 		company: "ABC Inc.",
-// // 	},
-// // } as Request;
-
-// const req = {
-// 	query: {
-// 		limit: "AA",
-// 		skip: "AA",
-// 	},
-// } as unknown as Request;
-
-// const res = {
-// 	status: jest.fn(),
-// 	json: () => {},
-// } as unknown as Response;
-
-// const next = jest.fn();
-
-// // const mockResponse = () => {
-// // 	const res = {
-// // 		status: jest.fn().mockReturnValue(this),
-// // 		json: jest.fn().mockReturnValue(this),
-// // 	};
-// // 	return res;
-// // };
-
-// // Tests for the root path
-// describe("Testing error middleware", () => {
-// 	test("returns 500", async () => {
-// 		// const req = mockResponse;
-// 		const controller = new IndexController();
-// 		// controller.index(req);
-// 		const result = controller.index(req, res, next);
-// 		expect(result).toBe(1);
-// 	});
-// });
--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -2,16 +2,20 @@
 // This file is referenced by the index.route.ts ROUTE which uses IndexController as its CONTROLLER
 
 import { NextFunction, Request, Response } from "express";
+import HttpException from "../exceptions/HttpException";
 
 class IndexController {
-	public index = (req: Request, res: Response): any => {
-		res.json({ hello: "world" });
-		// try {
-		// 	// res.sendStatus(200);
-		// } catch (error) {
-		// 	// next(error);
-		// 	return 1;
-		// }
+	public index = (req: Request, res: Response, next: NextFunction): void => {
+		try {
+			// check if there was a .error field in the request body
+			if (!req?.body?.error) {
+				res.status(200).json({ hello: "world" });
+			} else {
+				next(new HttpException(500, "you messed up"));
+			}
+		} catch (error) {
+			next(error);
+		}
 	};
 	public pageNotFound = (req: Request, res: Response, next: NextFunction): void => {
 		try {
