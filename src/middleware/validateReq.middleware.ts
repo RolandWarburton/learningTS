1f663b19007f3c0578d83697ecefda6122ef5909
RolandWarburton <warburtonroland@gmail.com>
2021-07-06 20:54:10 +1000

    refactor validateReq middleware

--- src/middleware/validateReq.middleware.ts
+++ src/middleware/validateReq.middleware.ts
@@ -1,18 +1,17 @@
 import { NextFunction, Request, Response, RequestHandler } from "express";
-import Ajv from "ajv";
-import { JSONSchemaType } from "ajv";
+import { ValidateFunction } from "ajv";
 import HttpException from "../exceptions/HttpException";
 
 type Value = "body" | "query" | "params";
 
-// when we call this we need to tell it the type we expect the schema to be
-// for example in user.route.ts: `validateRequest<UserRequest>("params", userRequestSchema)`
-function validationMiddleware<Type>(value: Value, schema: JSONSchemaType<Type>): RequestHandler {
+// when we call this we need to tell it the type for the validate function
+// for example in user.route.ts: `validateRequest<UserRequest>("params", new Ajv().compile(mySchema);)`
+function validationMiddleware<Type>(
+	value: Value,
+	validate: ValidateFunction<Type>
+): RequestHandler {
 	// This is sort of like a factory pattern that returns some middleware function that can validate any <Type> of schema passed to it.
 	return (req: Request, res: Response, next: NextFunction) => {
-		// compile the schema for use
-		const validate = new Ajv().compile(schema);
-
 		// run a validation check of the request ("body"/"query"/"params") against the schema we gave it
 		if (validate(req[value])) next();
 		else next(new HttpException(400, `wrong body from validate middleware`));
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // This is just ONE route. A new file is created for every route
 // The Route contains an userController which is a standard-ish MVC controller
 // 		The job for userController is to actually run some code for the request
@@ -7,21 +9,22 @@ import UserController from "../controllers/user.controller";
 import Route from "../interfaces/routes.interface";
 import userRequestSchema, { UserRequest } from "../models/requests/user_id";
 import validateRequest from "../middleware/validateReq.middleware";
+import Ajv from "ajv";
 
 class UserRoute implements Route {
 	public path = "/user/:id";
 	public router = Router();
 	public userController = new UserController();
+	private validator = new Ajv().compile(userRequestSchema);
 
 	constructor() {
 		this.initializeRoutes();
 	}
 
-	// the `userRequestSchema` is a JSONSchemaType<UserRequest> object for AJV to create a validation function
 	private initializeRoutes() {
 		this.router.get(
 			`${this.path}`,
-			validateRequest<UserRequest>("params", userRequestSchema),
+			validateRequest<UserRequest>("params", this.validator),
 			this.userController.user
 		);
 	}
